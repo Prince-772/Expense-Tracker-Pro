56f78d2d87cfce193c53b5979948ea1f935a47ae
Prince-772 <pkaddon772@gmail.com>
2025-04-05 18:55:55 +0530

    testing

--- handlers/cookiesHandlers.js
+++ handlers/cookiesHandlers.js
@@ -2,14 +2,14 @@ const clearJWTtoken = (res) => {
   res.clearCookie("accessToken", {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite:"lax",
+    sameSite:"none",
   });
 };
 const setJWTtoken = (res, newToken) => {
   res.cookie("accessToken", newToken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite:"lax",
+    sameSite:"none",
   });
 };
 
