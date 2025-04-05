1ee7fe1567315103c8a87452a590264ccf7dbf51
Prince-772 <pkaddon772@gmail.com>
2025-04-05 16:29:39 +0530

    Minnor changes

--- handlers/cookiesHandlers.js
+++ handlers/cookiesHandlers.js
@@ -2,14 +2,14 @@ const clearJWTtoken = (res) => {
   res.clearCookie("accessToken", {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production"?"none":"lax",
+    sameSite: process.env.NODE_ENV === "production"?"strict":"lax",
   });
 };
 const setJWTtoken = (res, newToken) => {
   res.cookie("accessToken", newToken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production"?"none":"lax",
+    sameSite: process.env.NODE_ENV === "production"?"strict":"lax",
   });
 };
 
